e73cdf3260c43926c4a9b0168cd4860214558189
root <fdfpy@ybb.ne.jp>
2022-09-10 16:44:10 +0900

    “最初のコミ

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+###下記の処理は、Raspi3のdocker conatiner上で実施する。
+
+cp /home/vrctest/node-js-getting-started/public/vrchart.mp4 ./vrchart.mp4
+cp /home/vrctest/node-js-getting-started/public/toraku.mp4 ./toraku.mp4
+cp /home/vrctest/node-js-getting-started/public/kobetsu.mp4 ./kobetsu.mp4
+cp /home/vrctest/node-js-getting-started/public/zadankai/zslide.mp4 ./zslide.mp4

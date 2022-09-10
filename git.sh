7c77f7e7be31f7e27d0ff0801f5f1b6b4db914bc
fdfpy <nwydw033@yahoo.ne.jp>
2022-09-10 17:10:08 +0900

    T

--- git.sh
+++ git.sh
@@ -3,15 +3,15 @@
 ###下記の処理は、Raspi3のdocker conatiner上で実施する。
 
 echo "1"
-cp /home/vrctest/node-js-getting-started/public/vrchart.mp4 ./vrchart.mp4
-cp /home/vrctest/node-js-getting-started/public/toraku.mp4 ./toraku.mp4
-cp /home/vrctest/node-js-getting-started/public/kobetsu.mp4 ./kobetsu.mp4
-cp /home/vrctest/node-js-getting-started/public/zadankai/zslide.mp4 ./zslide.mp4
+cp /home/pi//vrctest/node-js-getting-started/public/vrchart.mp4 ./vrchart.mp4
+cp /home/pi/vrctest/node-js-getting-started/public/toraku.mp4 ./toraku.mp4
+cp /home/pi/vrctest/node-js-getting-started/public/kobetsu.mp4 ./kobetsu.mp4
+cp /home/pi/vrctest/node-js-getting-started/public/zadankai/zslide.mp4 ./zslide.mp4
 
-echo "2"
-git add .
-echo "3"
-git commit -m  "T"
-echo "4"
-git push -u origin master
-echo "5"
+##echo "2"
+##git add .
+##echo "3"
+##git commit -m  "T"
+##echo "4"
+##git push -u origin master
+##echo "5"

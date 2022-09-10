6a53aa2fde87ec326ebd716d80afd454a8841600
fdfpy <nwydw033@yahoo.ne.jp>
2022-09-10 17:22:31 +0900

    T

--- git.sh
+++ git.sh
@@ -3,12 +3,12 @@
 ###下記の処理は、Raspi3のdocker conatiner上で実施する。
 
 
-cp /home/pi//vrctest/node-js-getting-started/public/vrchart.mp4 ./vrchart.mp4
-cp /home/pi/vrctest/node-js-getting-started/public/toraku.mp4 ./toraku.mp4
-cp /home/pi/vrctest/node-js-getting-started/public/kobetsu.mp4 ./kobetsu.mp4
-cp /home/pi/vrctest/node-js-getting-started/public/zadankai/zslide.mp4 ./zslide.mp4
+cp /home/pi//vrctest/node-js-getting-started/public/vrchart.mp4 /home/pi/vrctest/fdfpy.github.io/vrchart.mp4
+cp /home/pi/vrctest/node-js-getting-started/public/toraku.mp4 /home/pi/vrctest/fdfpy.github.io/toraku.mp4
+cp /home/pi/vrctest/node-js-getting-started/public/kobetsu.mp4 /home/pi/vrctest/fdfpy.github.io/kobetsu.mp4
+cp /home/pi/vrctest/node-js-getting-started/public/zadankai/zslide.mp4 /home/pi/vrctest/fdfpy.github.io/zslide.mp4
 
-
+cd /home/pi/vrctest/fdfpy.github.io
 git add .
 git commit -m  "T"
 git push -u origin master

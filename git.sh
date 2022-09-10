972fc1cb910cd6b293d190d80f7e34dba253e036
root <fdfpy@ybb.ne.jp>
2022-09-10 16:48:58 +0900

    T

--- git.sh
+++ git.sh
@@ -6,3 +6,7 @@ cp /home/vrctest/node-js-getting-started/public/vrchart.mp4 ./vrchart.mp4
 cp /home/vrctest/node-js-getting-started/public/toraku.mp4 ./toraku.mp4
 cp /home/vrctest/node-js-getting-started/public/kobetsu.mp4 ./kobetsu.mp4
 cp /home/vrctest/node-js-getting-started/public/zadankai/zslide.mp4 ./zslide.mp4
+
+git add .
+git commit -m  "T"
+git push -u origin master

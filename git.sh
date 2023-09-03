eb33df03ab8d1638456228cf344acd600459c7db
fdfpy <nwydw033@yahoo.ne.jp>
2023-09-03 17:57:23 +0900

    T

--- git.sh
+++ git.sh
@@ -10,6 +10,7 @@ cp /home/pi/vrctest/node-js-getting-started/public/zadankai/zslide.mp4 /home/pi/
 cp /home/pi/vrctest/node-js-getting-started/public/zadankai_guest/zslide_guest.mp4 /home/pi/vrctest/fdfpy.github.io/zslide_guest.mp4
 cp /home/pi/vrctest/node-js-getting-started/public/zadankai_fy/zslide_fy.mp4 /home/pi/vrctest/fdfpy.github.io/zslide_fy.mp4
 cp /home/pi/vrctest/node-js-getting-started/public/zadankai_s/zslide_s.mp4 /home/pi/vrctest/fdfpy.github.io/zslide_s.mp4
+cp /home/pi/vrctest/node-js-getting-started/public/zadankai_fx/zslide_fx.mp4 /home/pi/vrctest/fdfpy.github.io/zslide_fx.mp4
 cd /home/pi/vrctest/fdfpy.github.io
 git add .
 git commit -m  "T"
